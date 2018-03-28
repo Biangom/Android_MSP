39cea9b872807dd79381d14b5b40dae0fb11aae8
Biangom <kss7858@naver.com>
2018-03-29 01:48:47 +0900

    위치등록전까지 구현

--- 0327_basicTest/app/src/main/java/omg/techdown/a0327_basictest/MainActivity.java
+++ 0327_basicTest/app/src/main/java/omg/techdown/a0327_basictest/MainActivity.java
@@ -1,6 +1,8 @@
 package omg.techdown.a0327_basictest;
 
 import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,53 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
 
-    public class Data {
-        String name;
-        float lat;
-        float lon;
-        int rad;
 
-        public Data(String name, float lat, float lon, int rad) {
-            this.name = name;
-            this.lat = lat;
-            this.lon = lon;
-            this.rad = rad;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public float getLat() {
-            return lat;
-        }
-
-        public void setLat(float lat) {
-            this.lat = lat;
-        }
-
-        public float getLon() {
-            return lon;
-        }
-
-        public void setLon(float lon) {
-            this.lon = lon;
-        }
-
-        public int getRad() {
-            return rad;
-        }
-
-        public void setRad(int rad) {
-            this.rad = rad;
-        }
-    }
 
     ArrayList<Data> dataList = new ArrayList<Data>();
+    ArrayList<Data> subTempList; // Sub activity에서 가져올 리스트
+    ArrayList<Data> addTempList; // Add activity에서 가져올 리스트
 
     Button btnNext;
     TextView[] idName = new TextView[3];
@@ -78,11 +38,16 @@ public class MainActivity extends AppCompatActivity {
         idName[1] = (TextView)findViewById(R.id.IdName2);
         idName[2] = (TextView)findViewById(R.id.IdName3);
 
-        for(int i = 0; i < 2; i++) {
-            // ArrayList에다가 넣을꺼 테스트
-            Data temp = new Data(Integer.toString(i), 1 , 1,5);
-            dataList.add(temp);
-        }
+        Data temp1 = new Data("gym", 1 , 1,5);
+        dataList.add(temp1);
+        Data temp2 = new Data("class room", 2 , 3,15);
+        dataList.add(temp2);
+
+//        for(int i = 0; i < 2; i++) {
+//            // ArrayList에다가 넣을꺼 테스트
+//            Data temp = new Data(Integer.toString(i), 1 , 1,5);
+//            dataList.add(temp);
+//        }
 
 //        // addActivitiy에서 보내준 intent를 받는다.
 //        Intent intent = getIntent(); // add인텐트에서 갖고온거
@@ -90,33 +55,57 @@ public class MainActivity extends AppCompatActivity {
 //        idName1.setText(str);
 
         btnNext = (Button)findViewById(R.id.IdBtnNext);
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //만약 3개가 꽉찼다면 입력을 받으면 안됀다.
-                if(dataList.size() == 3) {
-                    Toast.makeText(MainActivity.this, "요소가 꽉찼습니다!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //다음 패이지로 화면을 전환
-                // 화면을 전환할때 사용하는 클래스 그것이 Intent 클래스이다.
-                // 첫번쨰 파라미터는 이동 전 액티비티, 두번쨰 파라미터는 이동할 액티비티
-                // 이 인텐트는 a에서 b로 이동한다는 화면 전환 정보를 가지고 있다./
-                Intent intent = new Intent(MainActivity.this, AddActivity.class );
-                // 화면 전환하기!!
-                startActivityForResult(intent, 0);
-            }
-
-
-        });
 
         for(int i = 0; i< dataList.size() ; i++) {
             idName[i].setText(dataList.get(i).name);
         }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        for(int i = 0; i < 3; i++) {
+            if(i < dataList.size())
+                idName[i].setText(dataList.get(i).name);
+            else
+                idName[i].setText("");
+        }
+    }
+
+    public void onClickAdd(View view) {
+        if(dataList.size() == 3) {
+            Toast.makeText(MainActivity.this, "요소가 꽉찼습니다!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //다음 패이지로 화면을 전환
+        // 화면을 전환할때 사용하는 클래스 그것이 Intent 클래스이다.
+        // 첫번쨰 파라미터는 이동 전 액티비티, 두번쨰 파라미터는 이동할 액티비티
+        // 이 인텐트는 a에서 b로 이동한다는 화면 전환 정보를 가지고 있다.
+        Intent intent = new Intent(MainActivity.this, AddActivity.class );
+
+        intent.putParcelableArrayListExtra("list1", dataList);
+        // 화면 전환하기!!
+        startActivityForResult(intent, 0);
 
     }
+
+    public void onClickSub(View view){
+        if(dataList.size() == 0) {
+            Toast.makeText(MainActivity.this, "요소가 하나도 없습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //다음 패이지로 화면을 전환
+        // 화면을 전환할때 사용하는 클래스 그것이 Intent 클래스이다.
+        // 첫번쨰 파라미터는 이동 전 액티비티, 두번쨰 파라미터는 이동할 액티비티
+        // 이 인텐트는 a에서 b로 이동한다는 화면 전환 정보를 가지고 있다./
+        Intent intent = new Intent(MainActivity.this, SubActivity.class );
+        intent.putParcelableArrayListExtra("list2", dataList);
+
+        // 화면 전환하기!!
+        // Sub는 requestCode는 1이다.
+        startActivityForResult(intent, 1);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -132,37 +121,28 @@ public class MainActivity extends AppCompatActivity {
         //requestCode에 맞는 분기실행
         switch (requestCode) {
             case 0: // AddActivity에서 0으로 해놓았다.
-                String str = intent.getStringExtra("result_msg"); // msg키값에 해당하는 거 받기
-                Data temp = new Data(str, 1, 1, 1);
-                dataList.add(temp);
-                idName[dataList.size()-1].setText(temp.name);
-                // 이미 이전에 3개가 꽉차면 실행하지 않기로해서 따로 예외처리는 안했다.
+                addTempList = intent.getParcelableArrayListExtra("addlist");
+                dataList.clear();
+                for(int i = 0; i < addTempList.size(); i++ ) {
+                    dataList.add(addTempList.get(i));
+                }
+
                 break;
 
-
+            case 1: // SubActivity는 1로 해놓았다
+                subTempList = intent.getParcelableArrayListExtra("sublist");
+                dataList.clear();
+                for(int i = 0; i < subTempList.size(); i++ ) {
+                    dataList.add(subTempList.get(i));
+                }
+                break;
             default:
 
                 break;
-
         }
 
     }
 
-    public void onClickSub(View view){
-        if(dataList.size() == 0) {
-            Toast.makeText(MainActivity.this, "요소가 하나도 없습니다.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        //다음 패이지로 화면을 전환
-        // 화면을 전환할때 사용하는 클래스 그것이 Intent 클래스이다.
-        // 첫번쨰 파라미터는 이동 전 액티비티, 두번쨰 파라미터는 이동할 액티비티
-        // 이 인텐트는 a에서 b로 이동한다는 화면 전환 정보를 가지고 있다./
-        Intent intent = new Intent(MainActivity.this, SubActivity.class );
-        // 화면 전환하기!!
-        startActivityForResult(intent, 1);
-
-
-    }
 
 
 
--- 0327_basicTest/app/src/main/java/omg/techdown/a0327_basictest/SubActivity.java
+++ 0327_basicTest/app/src/main/java/omg/techdown/a0327_basictest/SubActivity.java
@@ -1,13 +1,83 @@
 package omg.techdown.a0327_basictest;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class SubActivity extends AppCompatActivity {
+
+    ArrayList<Data> _dataList;
+    String inputName;
+
+    TextView[] idName = new TextView[3];
+    EditText EditName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        idName[0] = (TextView) findViewById(R.id.IdTv1);
+        idName[1] = (TextView) findViewById(R.id.IdTv2);
+        idName[2] = (TextView) findViewById(R.id.IdTv3);
+        EditName = (EditText) findViewById(R.id.IdEditText);
+
+
+
+        // Main액티비티에서 보내준 intent 받기
+        Intent intent = getIntent();
+
+        _dataList = intent.getParcelableArrayListExtra("list2");
+
+        for(int i = 0; i < _dataList.size() ; i++)
+            idName[i].setText(_dataList.get(i).name);
+    }
+
+    public void onClickSub(View view) {
+        boolean DidRemove = false; // 같은 이름이 있다면 루프를 돌때 true로 바꿔질거임
+        inputName = EditName.getText().toString();
+
+        // 예외처리 (만약 데이터가 입력되지 않았다면 빈 것이다._
+        if(EditName.getText().toString().equals("")) {
+            Toast.makeText(SubActivity.this, "이름을 입력해주세요", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        for(int i = 0; i < _dataList.size(); i++) {
+            if(_dataList.get(i).name.equals(inputName)) {
+                _dataList.remove(i);
+                DidRemove = true;
+            }
+        }
+
+        // 예외처리 (만약 삭제되지 않았다면 해당하는 이름의 데이터가 없는 것이다.)
+        if(DidRemove == false) {
+            Toast.makeText(SubActivity.this, "해당하는 이름의 데이터가 없습니다.", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        else {
+            Toast.makeText(SubActivity.this, "삭제완료", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SubActivity.this, MainActivity.class );
+
+            intent.putParcelableArrayListExtra("sublist", _dataList);
+
+//        intent.putExtra("result_msg", etName.getText().toString()); // t는 키 값임
+            setResult(RESULT_OK, intent ); // 안전하게 보냈다
+
+            finish();// 현재 액티비티 종료
+
+        }
+
+
     }
 }
